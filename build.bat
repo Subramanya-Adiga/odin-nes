6039b29d375dce3484c5fa67fc58460fc4588f3a
Subramanya <subramanya.adiga4@protonmail.com>
2025-02-05 15:19:35 +0530

    Removed Subsystem Flag

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 
-odin build src -target-features:"avx2" -lld -debug -subsystem:windows -out:"build/odin-nes.exe"
+odin build src -target-features:"avx2" -debug -o:"none" -out:"build/odin-nes.exe"
